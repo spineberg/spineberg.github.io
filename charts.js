275a05f6fd22b950aea72305750a48210496bfa1
spineberg <scottkw.mob@gmail.com>
2017-05-07 00:54:33 -0500

    Added index page, CSS fixes
    Index page includes 8values result readouts for individual spreadsheet rows

--- charts.js
+++ charts.js
@@ -1,4 +1,6 @@
 var resultsArray = [];
+var avgResult = [];
+var medResult = [];
 var spData = null;
 
 function doData(json){
@@ -33,7 +35,6 @@ var sortColumn = 0;
 function getArrayAvg(){
 var i;
 var col = 1;
-var avg = [];
 
 for(col ; col < 5 ; col++){
 	var sum = 0;
@@ -41,62 +42,117 @@ for(col ; col < 5 ; col++){
 	for(i = 0 ; i < resultsArray.length ; i++){
 		sum += parseFloat(resultsArray[i][col]);
 	}	
-	avg[col-1] = (sum / i).toFixed(1);
+	avgResult[col-1] = (sum / i).toFixed(1);
 }
-	return avg;
-}
-
-function getArrayMed(){
-	var i;
-	var col = 1;
-	var med = [];
-	
-	for(col ; col < 5 ; col++){
-		var values = [];
-		
-		for(i = 0 ; i < resultsArray.length ; i++){
-			values[i] = parseFloat(resultsArray[i][col]);
-			}
-		
-		values.sort(sortFunction);
-		
-		var half = Math.floor(resultsArray.length/2);
-		
-		if(values.length % 2){
-			med[col-1] = (values[half]).toFixed(1);
-		}else{
-			med[col-1] = ((values[half+1] + values[half]) / 2.0).toFixed(1);
-		}
-	}
-	return med;
 }
 
 function getLabel(val, axis){
-	var ary = new Array();
+	var labelArray = new Array();
 	
-	if(axis === 1) { ary = ["Communist","Socialist","Social","Centrist","Market","Capitalist","Laissez-Faire"];} else
-		if(axis === 2) { ary = ["Cosmopolitan","Internationalist","Peaceful","Balanced","Patriotic","Nationalist","Chauvinist"];} else
-		if(axis === 3) { ary = ["Anarchist","Libertarian","Liberal","Moderate","Statist","Authoritarian","Totalitarian"];} else
-		if(axis === 4) { ary = ["Revolutionary","Very Progressive","Progressive","Neutral","Traditional","Very Traditional","Reactionary"];}
+	if(axis === 1) { labelArray = ["Communist","Socialist","Social","Centrist","Market","Capitalist","Laissez-Faire"];} else
+		if(axis === 2) { labelArray = ["Cosmopolitan","Internationalist","Peaceful","Balanced","Patriotic","Nationalist","Chauvinist"];} else
+		if(axis === 3) { labelArray = ["Anarchist","Libertarian","Liberal","Moderate","Statist","Authoritarian","Totalitarian"];} else
+		if(axis === 4) { labelArray = ["Revolutionary","Very Progressive","Progressive","Neutral","Traditional","Very Traditional","Reactionary"];}
 	
 	if (val > 100) { return "" } else
-        if (val > 90) { return ary[0] } else
-        if (val > 75) { return ary[1] } else
-        if (val > 60) { return ary[2] } else
-        if (val >= 40) { return ary[3] } else
-        if (val >= 25) { return ary[4] } else
-        if (val >= 10) { return ary[5] } else
-        if (val >= 0) { return ary[6] } else
+        if (val > 90) { return labelArray[0] } else
+        if (val > 75) { return labelArray[1] } else
+        if (val > 60) { return labelArray[2] } else
+        if (val >= 40) { return labelArray[3] } else
+        if (val >= 25) { return labelArray[4] } else
+        if (val >= 10) { return labelArray[5] } else
+        if (val >= 0) { return labelArray[6] } else
         	{return ""}
 
 }
 
-function generateSummary(){
-	//var avgRes = getArrayAvg();
-	//var medRes = getArrayMed();
+    function setIndexBarValue(name, value) {
+        innerel = document.getElementById(name)
+        outerel = document.getElementById("index-bar-" + name)
+        outerel.style.width = (value + "%")
+        innerel.innerHTML = (value + "%")
+        if (value < 10) {
+            innerel.style.visibility = "hidden"
+        }else{
+			innerel.style.visibility = "visible";
+		}
+    }
+
+function generateIndex(){
 	
-	sortColumn+=1;
+	indexDiv = document.getElementById("index-menu");
 	
+	for (var i in resultsArray){
+		var newIndexButton = document.createElement('div');
+		newIndexButton.id = resultsArray[i][0].replace(/\s/g, '') + "-index";	
+		newIndexButton.innerHTML = ('<button class="indexlinks" onclick="openResult(event, ' + i + ')">' + resultsArray[i][0] + '</button>');
+		indexDiv.appendChild(newIndexButton);
+	}
+	sortColumn+=1;	
+}
+
+function openResult(evt, row){
+		var nametag = document.getElementById("index-nametag");
+	
+	    indexlinks = document.getElementsByClassName("indexlinks");
+    for (i = 0; i < indexlinks.length; i++) {
+        indexlinks[i].className = indexlinks[i].className.replace(" active", "");
+    }
+    
+	var indexResArray = [];
+	var n = 0;
+	
+	if(row >= 0){
+		indexResArray = resultsArray[row];
+		nametag.innerHTML = "Results for " + resultsArray[row][0]
+	}else if(row == -1){
+		n-=1;
+		indexResArray = avgResult;
+		nametag.innerHTML = "Average Result";
+	}else if(row == -2){
+		n-=1;
+		indexResArray = medResult;
+		nametag.innerHTML = "Median Result";
+	}
+	
+	var equality =	(indexResArray[n+1]);
+	var wealth = 	((100-indexResArray[n+1]).toFixed(1));
+	var peace =		(indexResArray[n+2]);
+	var might =		((100-indexResArray[n+2]).toFixed(1));
+	var liberty =	(indexResArray[n+3]);
+	var authority =	((100-indexResArray[n+3]).toFixed(1));
+	var progress =	(indexResArray[n+4]);
+	var tradition =	((100-indexResArray[n+4]).toFixed(1));
+	
+	setIndexBarValue("equality", equality);
+    setIndexBarValue("wealth", 	wealth);
+    setIndexBarValue("peace", peace);
+    setIndexBarValue("might", might);
+    setIndexBarValue("liberty", liberty);
+    setIndexBarValue("authority", authority);
+    setIndexBarValue("progress", progress);
+    setIndexBarValue("tradition", tradition);
+    
+    document.getElementById("econ-label").innerHTML = getLabel(equality, 1);
+    document.getElementById("dipl-label").innerHTML = getLabel(peace, 2);
+	document.getElementById("govt-label").innerHTML = getLabel(liberty, 3);
+	document.getElementById("soci-label").innerHTML = getLabel(progress, 4);
+	
+	ideology = "";
+    ideodist = Infinity;
+    for (var i = 0; i < ideologies.length; i++) {
+        dist = 0;
+        dist += Math.pow(Math.abs(ideologies[i].stats.econ - equality), 2);
+        dist += Math.pow(Math.abs(ideologies[i].stats.govt - liberty), 2);
+        dist += Math.pow(Math.abs(ideologies[i].stats.dipl - peace), 1.73856063);
+        dist += Math.pow(Math.abs(ideologies[i].stats.scty - progress), 1.73856063);
+        if (dist < ideodist) {
+            ideology = ideologies[i].name;
+            ideodist = dist;
+        }
+    }
+    document.getElementById("ideology-label").innerHTML = ideology;
+    evt.currentTarget.className += " active";
 }
 
 function generateChart(cat, col){
@@ -127,7 +183,7 @@ function generateChart(cat, col){
 
 	for(var i in resultsArray){
 		// Create label based on recorded value and current axis.
-		var tooltip = getLabel(resultsArray[i][col],col);
+		var tooltipText = getLabel(resultsArray[i][col],col);
 		
 		var leftBarResult, rightBarResult;
 		
@@ -142,6 +198,11 @@ function generateChart(cat, col){
 	
 		// Create nametag and bar wrapper.
 		var newBar = document.createElement('div');
+		var tooltipElement = document.createElement('span');
+		
+		tooltipElement.className = "tooltiptext";
+		tooltipElement.innerHTML = tooltipText;
+		
 		newBar.id = resultsArray[i][0].replace(/\s/g, '') + "-" + cat;						// If the nametag contains whitespace, remove it to make the div id less of a headache.
 		newBar.className = "axis";
 		newBar.innerHTML = ('<p class="nametag">' + resultsArray[i][0] + '</p>');			// Leave whitespace in for the visible names, though.
@@ -152,7 +213,6 @@ function generateChart(cat, col){
 		innerBarLeft.id = resultsArray[i][0].replace(/\s/g, '') + "-bar-" + leftVal;
 		innerBarLeft.className = "bar " + leftVal;
 		innerBarLeft.style.width = (leftBarResult + "%");
-		innerBarLeft.title = tooltip;
 		innerBarLeft.innerHTML = (leftBarResult + "%");
 		document.getElementById(newBar.id).appendChild(innerBarLeft);
 		
@@ -160,7 +220,6 @@ function generateChart(cat, col){
 		innerBarRight.id = resultsArray[i][0].replace(/\s/g, '') + "-bar-" + rightVal;
 		innerBarRight.className = "bar " + rightVal;
 		innerBarRight.style.width = (rightBarResult + "%");
-		innerBarRight.title = tooltip;
 		innerBarRight.innerHTML = (rightBarResult + "%");
 		document.getElementById(newBar.id).appendChild(innerBarRight);
 		
@@ -202,8 +261,11 @@ $(document).ready(function () {
 
 	resultsArray = readData();
 
+	//  	First sort alphabetically
 	resultsArray.sort(sortFunction);
-	generateSummary();
+	getArrayAvg();
+	//getArrayMed();
+	generateIndex();
 
 	resultsArray.sort(sortFunction);
 	generateChart("econ", 1);
@@ -216,4 +278,8 @@ $(document).ready(function () {
 	
 	resultsArray.sort(sortFunction);
 	generateChart("soci", 4);
+	
+	sortColumn = 0;
+	resultsArray.sort(sortFunction);
+	
   });
